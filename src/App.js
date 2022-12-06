d7f40756f3293f4bdedbe418a6c60b96db9d30ab
bigtedde <big.tedde@gmail.com>
2022-12-05 22:00:08 -0800

    Added various UI elements and added data.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+    <Navbar /> 
       <div className="content">
-        <h1>App Component</h1>
+        <Home />        
       </div>
     </div>
   );
-}
+ }
 
 export default App;
